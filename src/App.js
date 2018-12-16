464c1d4102160eaea15e60f57a124e2160334525
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-12-16 16:37:12 -0500

    add form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
+import Form from './components/Form';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      cheese: "ham"
+      cheese: "ham",
+      dataFromChild: "no data from child yet"
     }
+
+    this.getDataFromChild = this.getDataFromChild.bind(this);
+  }
+
+  getDataFromChild(dataObject){
+    console.log(dataObject)
+    this.setState({ dataFromChild: dataObject})
   }
 
   render() {
     return (
-      <div>
+      <>
         <p>{this.state.cheese}</p>
-      </div>
+        <Form sendDataUp={this.getDataFromChild}/>
+        <h1
+          style={{ textAlign:"center", backgroundColor:"lightSalmon" }}
+          >
+          {this.state.dataFromChild}
+          </h1>
+      </>
     );
   }
 }
---
+++ src/components/Form.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class Form extends React.Component {
+ constructor(props) {
+   super(props);
+   this.state = {value: ''};
+
+   this.handleChange = this.handleChange.bind(this);
+   this.handleSubmit = this.handleSubmit.bind(this);
+ }
+
+ componentDidMount(){
+   console.log(this.props)
+ }
+
+ handleChange(event) {
+   console.log("setting state on change: " + event.target.value)
+   this.setState({ value: event.target.value});
+ }
+
+ handleSubmit(event) {
+   console.log('state on submit: ' + this.state.value);
+   this.props.sendDataUp(this.state.value);
+   event.preventDefault();
+ }
+
+ render() {
+   return (
+    <>
+     <form onSubmit={this.handleSubmit}>
+       <input
+         type="text"
+         value={this.state.value}
+         onChange={this.handleChange} />
+       <input
+        type="submit"
+        value="Submit" />
+     </form>
+    <h1 style={{ textAlign: "center" }}>{this.state.value}</h1>
+     </>
+   );
+ }
+}
+
+export default Form;
